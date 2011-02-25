3e8fa2bec7e5d1b13cebc2bc32e59f658fd6c21e
Sergey Potapov <potapov.s.a@gmail.com>
2011-02-25 15:48:20 +0300

    Для совместимости с Transifex из имени каталога с локализацией удален код страны.

---
+++ translations/uk/gmailnotify.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk" sourcelanguage="en">
+<context>
+    <name>GmailNotify</name>
+    <message>
+        <source>GMail Notifications</source>
+        <translation>Сповіщення GMail </translation>
+    </message>
+    <message>
+        <source>New e-mail</source>
+        <translation>Новий e-mail</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n unread message(s)</source>
+        <translation>
+            <numerusform>У вас %n непрочитане повідомлення</numerusform>
+            <numerusform>У вас %n непрочитаних повідомлень</numerusform>
+            <numerusform>У вас %n непрочитаних повідомлень</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>New e-mail for %1</source>
+        <translation>Новий e-mail для %1</translation>
+    </message>
+    <message>
+        <source>Notify of new e-mails in Google Mail</source>
+        <translation>Сповіщає про нову пошту від Google Mail</translation>
+    </message>
+    <message>
+        <source>Supports the notifications of new e-mails in Google Mail</source>
+        <translation>Підтримує сповіщення про нову пошту від Google Mail</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n new unread message(s)</source>
+        <translation>
+            <numerusform>У вас %n непрочитане повідомлення</numerusform>
+            <numerusform>У вас %n непрочитаних повідомлень</numerusform>
+            <numerusform>У вас %n непрочитаних повідомлень</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитане повідомлення&amp;lt;/b&amp;gt;:</numerusform>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитане повідомлення&amp;lt;/b&amp;gt;:</numerusform>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитаних повідомлень&amp;lt;/b&amp;gt;:</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>NotifyGmailDialog</name>
+    <message>
+        <source>Open web interface of Google Mail?</source>
+        <translation>Відкрити веб-інтерфейс Google Mail?</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитане повідомлення&amp;lt;/b&amp;gt;:</numerusform>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитане повідомлення&amp;lt;/b&amp;gt;:</numerusform>
+            <numerusform>У вас &amp;lt;b&amp;gt;%n непрочитаних повідомлень&amp;lt;/b&amp;gt;:</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>GMail Notify</source>
+        <translation>Оповіщення GMail </translation>
+    </message>
+</context>
+</TS>
