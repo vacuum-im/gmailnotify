4159b536c25adac2ee6ce8ff081c0c8829ded799
Crying Angel <cangel@inbox.ru>
2011-03-26 22:04:12 +0200

    add CMake build system.

---
+++ translations/de/gmailnotify.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>GmailNotify</name>
+    <message>
+        <source>GMail Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Notify of new e-mails in Google Mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New e-mail for %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New e-mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n unread message(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n new unread message(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Supports the notifications of new e-mails in Google Mail</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>NotifyGmailDialog</name>
+    <message>
+        <source>GMail Notify</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
