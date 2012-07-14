69d242c25b2f4d270a258d65eb59313f6cededaa
Sergey Potapov <potapov.s.a@gmail.com>
2012-07-14 14:24:19 +0400

    Добавлена английская локализация для удобства перевода в Transifex.

---
+++ translations/en/gmailnotify.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>GmailNotify</name>
+    <message>
+        <source>GMail Notifications</source>
+        <translation>GMail Notifications</translation>
+    </message>
+    <message>
+        <source>Notify of new e-mails in Google Mail</source>
+        <translation>Notify of new e-mails in Google Mail</translation>
+    </message>
+    <message>
+        <source>When receiving a new message in google mail</source>
+        <translation>When receiving a new message in google mail</translation>
+    </message>
+    <message>
+        <source>New e-mail for %1</source>
+        <translation>New e-mail for %1</translation>
+    </message>
+    <message>
+        <source>New e-mail</source>
+        <translation>New e-mail</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n unread message(s)</source>
+        <translation>
+            <numerusform>You have %n unread message(s)</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n new unread message(s)</source>
+        <translation>
+            <numerusform>You have %n new unread message(s)</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Supports the notifications of new e-mails in Google Mail</source>
+        <translation>Supports the notifications of new e-mails in Google Mail</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation>
+            <numerusform>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>NotifyGmailDialog</name>
+    <message>
+        <source>Open web interface of Google Mail?</source>
+        <translation>Open web interface of Google Mail?</translation>
+    </message>
+    <message>
+        <source>GMail Notify</source>
+        <translation>GMail Notify</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</source>
+        <translation>
+            <numerusform>You have &lt;b&gt;%n unread letter(s)&lt;/b&gt;:</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
